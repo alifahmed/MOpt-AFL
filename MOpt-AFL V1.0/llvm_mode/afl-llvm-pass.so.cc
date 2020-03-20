70c4526dd3d1fc5b37291667f9f8ba92d02567bd
Alif Ahmed <alifahmed@virginia.edu>
2020-03-20 05:06:08 -0400

    Fixed llvm-mode

--- MOpt-AFL V1.0/llvm_mode/afl-llvm-pass.so.cc
+++ MOpt-AFL V1.0/llvm_mode/afl-llvm-pass.so.cc
@@ -1,4 +1,20 @@
 /*
+ Copyright 2015 Google LLC All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at:
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+/*
  american fuzzy lop - LLVM-mode instrumentation pass
  ---------------------------------------------------
 
@@ -8,18 +24,9 @@
  LLVM integration design comes from Laszlo Szekeres. C bits copied-and-pasted
  from afl-as.c are Michal's fault.
 
- Copyright 2015, 2016 Google Inc. All rights reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at:
-
- http://www.apache.org/licenses/LICENSE-2.0
-
  This library is plugged into LLVM when invoking clang through afl-clang-fast.
  It tells the compiler to add code roughly equivalent to the bits discussed
  in ../afl-as.h.
-
  */
 
 #define AFL_LLVM_PASS
@@ -30,6 +37,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <vector>
 
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/IRBuilder.h"
@@ -37,8 +45,8 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/MDBuilder.h"
 #include "llvm/Support/Debug.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
 
@@ -66,179 +74,171 @@ public:
 char AFLCoverage::ID = 0;
 
 bool AFLCoverage::runOnModule(Module &M) {
+  LLVMContext &C = M.getContext();
 
-	LLVMContext &C = M.getContext();
+  IntegerType *   Int8Ty = IntegerType::getInt8Ty(C);
+  IntegerType *   Int32Ty = IntegerType::getInt32Ty(C);
 
-	IntegerType *Int8Ty = IntegerType::getInt8Ty(C);
-	IntegerType *Int32Ty = IntegerType::getInt32Ty(C);
+  char be_quiet = 0;
 
-	/* Show a banner */
+  if (isatty(2) && !getenv("AFL_QUIET")) {
 
-	char be_quiet = 0;
+    SAYF(cCYA "afl-llvm-pass" VERSION cRST " by <lszekeres@google.com>\n");
 
-	if (isatty(2) && !getenv("AFL_QUIET")) {
+  } else if (getenv("AFL_QUIET"))
 
-		SAYF(
-				cCYA "afl-llvm-pass " cBRI VERSION cRST " by <lszekeres@google.com>\n");
+    be_quiet = 1;
 
-	} else
-		be_quiet = 1;
+  /* Decide instrumentation ratio */
 
-	/* Decide instrumentation ratio */
+  char *       inst_ratio_str = getenv("AFL_INST_RATIO");
+  unsigned int inst_ratio = 100;
 
-	char *inst_ratio_str = getenv("AFL_INST_RATIO");
-	unsigned int inst_ratio = 100;
+  if (inst_ratio_str) {
 
-	if (inst_ratio_str) {
-		if (sscanf(inst_ratio_str, "%u", &inst_ratio) != 1 || !inst_ratio
-				|| inst_ratio > 100)
-			FATAL("Bad value of AFL_INST_RATIO (must be between 1 and 100)");
-	}
+    if (sscanf(inst_ratio_str, "%u", &inst_ratio) != 1 || !inst_ratio ||
+        inst_ratio > 100)
+      FATAL("Bad value of AFL_INST_RATIO (must be between 1 and 100)");
 
-	/* Get globals for the SHM region and the previous location. Note that
-	 __afl_prev_loc is thread-local. */
+  }
 
-	GlobalVariable *AFLValPtr = new GlobalVariable(M,
-			PointerType::get(Int8Ty, 0), false, GlobalValue::ExternalLinkage, 0,
-			"__afl_val_ptr");
+  /* Get globals for the SHM region and the previous location. Note that
+     __afl_prev_loc is thread-local. */
 
-	GlobalVariable *AFLIdxPtr = new GlobalVariable(M,
-			PointerType::get(Int32Ty, 0), false, GlobalValue::ExternalLinkage,
-			0, "__afl_idx_ptr");
+  GlobalVariable *AFLMapPtr =
+      new GlobalVariable(M, PointerType::get(Int8Ty, 0), false,
+                         GlobalValue::ExternalLinkage, 0, "__afl_area_ptr");
 
-	GlobalVariable *AFLPrevLoc1 = new GlobalVariable(M, Int32Ty, false,
-			GlobalValue::ExternalLinkage, 0, "__afl_prev_loc1", 0,
-			GlobalVariable::GeneralDynamicTLSModel, 0, false);
+  GlobalVariable *AFLIdxPtr =
+        new GlobalVariable(M, PointerType::get(Int32Ty, 0), false,
+                           GlobalValue::ExternalLinkage, 0, "__afl_idx_ptr");
 
-	GlobalVariable *AFLPrevLoc2 = new GlobalVariable(M, Int32Ty, false,
-			GlobalValue::ExternalLinkage, 0, "__afl_prev_loc2", 0,
-			GlobalVariable::GeneralDynamicTLSModel, 0, false);
+  GlobalVariable *AFLPrevLoc1 = new GlobalVariable(
+      M, Int32Ty, false, GlobalValue::ExternalLinkage, 0, "__afl_prev_loc1", 0,
+      GlobalVariable::GeneralDynamicTLSModel, 0, false);
 
-	GlobalVariable *AFLPrevLoc3 = new GlobalVariable(M, Int32Ty, false,
-			GlobalValue::ExternalLinkage, 0, "__afl_prev_loc3", 0,
-			GlobalVariable::GeneralDynamicTLSModel, 0, false);
+  GlobalVariable *AFLPrevLoc2 = new GlobalVariable(
+        M, Int32Ty, false, GlobalValue::ExternalLinkage, 0, "__afl_prev_loc2", 0,
+        GlobalVariable::GeneralDynamicTLSModel, 0, false);
 
-	/* Instrument all the things! */
+  GlobalVariable *AFLPrevLoc3 = new GlobalVariable(
+        M, Int32Ty, false, GlobalValue::ExternalLinkage, 0, "__afl_prev_loc3", 0,
+        GlobalVariable::GeneralDynamicTLSModel, 0, false);
 
-	int inst_blocks = 0;
+  /* Instrument all the things! */
 
-	for (auto &F : M) {
-		std::vector<BasicBlock::iterator> ips;
-		for (auto &bb : F) {
-			ips.push_back(bb.getFirstInsertionPt());
-		}
+  int inst_blocks = 0;
 
-		for (auto &IP : ips) {
-			if (AFL_R(100) >= inst_ratio)
-				continue;
+  for (auto &F : M){
+  		std::vector<BasicBlock::iterator> ips;
+  		for(auto &bb : F){
+  			ips.push_back(bb.getFirstInsertionPt());
+  		}
 
-			IRBuilder<> IRB(&(*IP));
+  		for (auto &IP : ips) {
 
-			unsigned int cur_loc = AFL_R(MAP_SIZE);
-			while ((cur_loc ^ (cur_loc >> 1)) == 0) {
-				cur_loc = AFL_R(MAP_SIZE);
-			}
+  			//BasicBlock::iterator IP = BB.getFirstInsertionPt();
+  			//Instruction * firstInstOrig = &(*IP);
+  			IRBuilder<> IRB(&(*IP));
 
-			ConstantInt *CurLoc = ConstantInt::get(Int32Ty, cur_loc);
+  			if (AFL_R(100) >= inst_ratio)
+  				continue;
 
-			/* Load prev_loc */
-			LoadInst *PrevLoc1 = IRB.CreateLoad(AFLPrevLoc1);
-			PrevLoc1->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
-			LoadInst *PrevLoc2 = IRB.CreateLoad(AFLPrevLoc2);
-			PrevLoc2->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
-			LoadInst *PrevLoc3 = IRB.CreateLoad(AFLPrevLoc3);
-			PrevLoc3->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
+  			/* Make up cur_loc */
 
-			Value *key = IRB.CreateXor(IRB.CreateXor(PrevLoc1, PrevLoc2),
-					IRB.CreateXor(PrevLoc3, CurLoc));
-			IRB.CreateStore(PrevLoc2, AFLPrevLoc3)->setMetadata(
-					M.getMDKindID("nosanitize"), MDNode::get(C, None));
-			IRB.CreateStore(PrevLoc1, AFLPrevLoc2)->setMetadata(
-					M.getMDKindID("nosanitize"), MDNode::get(C, None));
-			IRB.CreateStore(ConstantInt::get(Int32Ty, cur_loc >> 1),
-					AFLPrevLoc1)->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
+  			unsigned int cur_loc = AFL_R(MAP_SIZE);
+  			while((cur_loc ^ (cur_loc >> 1)) == 0){
+  				cur_loc = AFL_R(MAP_SIZE);
+  			}
 
-			/*load idx*/
-			LoadInst *IdxPtr = IRB.CreateLoad(AFLIdxPtr);
-			IdxPtr->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
-			Value *idxAddr = IRB.CreateGEP(Int32Ty, IdxPtr, key);
+  			ConstantInt *CurLoc = ConstantInt::get(Int32Ty, cur_loc);
 
-			//load index value
-			LoadInst *idxVal = IRB.CreateLoad(idxAddr);
-			idxVal->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
+  			/* Load prev_loc */
+  			LoadInst *PrevLoc1 = IRB.CreateLoad(AFLPrevLoc1);
+  			PrevLoc1->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			LoadInst *PrevLoc2 = IRB.CreateLoad(AFLPrevLoc2);
+  			PrevLoc2->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			LoadInst *PrevLoc3 = IRB.CreateLoad(AFLPrevLoc3);
+  			PrevLoc3->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
 
-			//check if index value is -1
-			Value *cond = IRB.CreateICmpEQ(ConstantInt::get(Int32Ty, -1),
-					idxVal);
+  			Value* key = IRB.CreateXor(IRB.CreateXor(PrevLoc1, PrevLoc2), IRB.CreateXor(PrevLoc3, CurLoc));
+  			IRB.CreateStore(PrevLoc2, AFLPrevLoc3)->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			IRB.CreateStore(PrevLoc1, AFLPrevLoc2)->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			IRB.CreateStore(ConstantInt::get(Int32Ty, cur_loc >> 1), AFLPrevLoc1)->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
 
-			//create then block
-			TerminatorInst *then = SplitBlockAndInsertIfThen(cond, &(*IP),
-					false, MDBuilder(C).createBranchWeights(1, 100000));
-			assert(dyn_cast<BranchInst>(then)->isUnconditional());
+  			/*load idx*/
+  			LoadInst *IdxPtr = IRB.CreateLoad(AFLIdxPtr);
+  			IdxPtr->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			Value *idxAddr = IRB.CreateGEP(Int32Ty, IdxPtr, key);
 
-			//instrument then block
-			IRB.SetInsertPoint(then);
-			LoadInst *cntVal = IRB.CreateLoad(IdxPtr);
-			cntVal->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
-			IRB.CreateStore(cntVal, idxAddr)->setMetadata(
-					M.getMDKindID("nosanitize"), MDNode::get(C, None));
-			IRB.CreateStore(IRB.CreateAdd(cntVal, ConstantInt::get(Int32Ty, 1)),
-					IdxPtr)->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
+  			//load index value
+  			LoadInst* idxVal = IRB.CreateLoad(idxAddr);
+  			idxVal->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
 
-			//instrument tail
-			IRB.SetInsertPoint(&(*IP));
-			PHINode *idx = IRB.CreatePHI(Int32Ty, 2);
-			idx->addIncoming(idxVal, idxVal->getParent());
-			idx->addIncoming(cntVal, cntVal->getParent());
+  			//check if index value is -1
+  			Value* cond = IRB.CreateICmpEQ(ConstantInt::get(Int32Ty, -1), idxVal);
 
-			LoadInst *MapPtr = IRB.CreateLoad(AFLValPtr);
-			MapPtr->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
-			Value *mapAddr = IRB.CreateGEP(Int8Ty, MapPtr, idx);
+  			//create then block
+  			TerminatorInst* then = SplitBlockAndInsertIfThen(cond, &(*IP), false, MDBuilder(C).createBranchWeights(1, 100000));
+  			assert(dyn_cast<BranchInst>(then)->isUnconditional());
 
-			LoadInst *mapVal = IRB.CreateLoad(mapAddr);
-			mapVal->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
-			IRB.CreateStore(IRB.CreateAdd(mapVal, ConstantInt::get(Int8Ty, 1)),
-					mapAddr)->setMetadata(M.getMDKindID("nosanitize"),
-					MDNode::get(C, None));
+  			//instrument then block
+  			IRB.SetInsertPoint(then);
+  			LoadInst* cntVal = IRB.CreateLoad(IdxPtr);
+  			cntVal->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			IRB.CreateStore(cntVal, idxAddr)->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			IRB.CreateStore(IRB.CreateAdd(cntVal, ConstantInt::get(Int32Ty, 1)), IdxPtr)->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
 
-			inst_blocks++;
-		}
-	}
 
-	/* Say something nice. */
+  			//instrument tail
+  			IRB.SetInsertPoint(&(*IP));
+  			PHINode* idx = IRB.CreatePHI(Int32Ty, 2);
+  			idx->addIncoming(idxVal, idxVal->getParent());
+  			idx->addIncoming(cntVal, cntVal->getParent());
 
-	if (!be_quiet) {
+  			LoadInst *MapPtr = IRB.CreateLoad(AFLMapPtr);
+  			MapPtr->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			Value *mapAddr = IRB.CreateGEP(Int8Ty, MapPtr, idx);
 
-		if (!inst_blocks)
-			WARNF("No instrumentation targets found.");
-		else
-			OKF("Instrumented %u locations (%s mode, ratio %u%%).", inst_blocks,
-					getenv("AFL_HARDEN") ?
-							"hardened" :
-							((getenv("AFL_USE_ASAN") || getenv("AFL_USE_MSAN")) ?
-									"ASAN/MSAN" : "non-hardened"), inst_ratio);
+  			LoadInst *mapVal = IRB.CreateLoad(mapAddr);
+  			mapVal->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+  			IRB.CreateStore(IRB.CreateAdd(mapVal, ConstantInt::get(Int8Ty, 1)), mapAddr)->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
 
-	}
-	return true;
+  			inst_blocks++;
+
+  		}
+  	}
+
+  /* Say something nice. */
+
+  if (!be_quiet) {
+
+    if (!inst_blocks)
+      WARNF("No instrumentation targets found.");
+    else
+      OKF("Instrumented %u locations (%s mode, ratio %u%%).", inst_blocks,
+          getenv("AFL_HARDEN")
+              ? "hardened"
+              : ((getenv("AFL_USE_ASAN") || getenv("AFL_USE_MSAN"))
+                     ? "ASAN/MSAN"
+                     : "non-hardened"),
+          inst_ratio);
+
+  }
+
+  return true;
+
 }
 
 static void registerAFLPass(const PassManagerBuilder&,
 		legacy::PassManagerBase &PM) {
+
 	PM.add(new AFLCoverage());
+
 }
 
 static RegisterStandardPasses RegisterAFLPass(
-		PassManagerBuilder::EP_OptimizerLast, registerAFLPass);
+		PassManagerBuilder::EP_ModuleOptimizerEarly, registerAFLPass);
 
 static RegisterStandardPasses RegisterAFLPass0(
 		PassManagerBuilder::EP_EnabledOnOptLevel0, registerAFLPass);
--- MOpt-AFL V1.0/llvm_mode/afl-llvm-rt.o.c
+++ MOpt-AFL V1.0/llvm_mode/afl-llvm-rt.o.c
@@ -1,23 +1,30 @@
 /*
- american fuzzy lop - LLVM instrumentation bootstrap
- ---------------------------------------------------
+  Copyright 2015 Google LLC All rights reserved.
 
- Written by Laszlo Szekeres <lszekeres@google.com> and
- Michal Zalewski <lcamtuf@google.com>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at:
 
- LLVM integration design comes from Laszlo Szekeres.
+    http://www.apache.org/licenses/LICENSE-2.0
 
- Copyright 2015, 2016 Google Inc. All rights reserved.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at:
+/*
+   american fuzzy lop - LLVM instrumentation bootstrap
+   ---------------------------------------------------
 
- http://www.apache.org/licenses/LICENSE-2.0
+   Written by Laszlo Szekeres <lszekeres@google.com> and
+              Michal Zalewski <lcamtuf@google.com>
 
- This code is the rewrite of afl-as.h's main_payload.
+   LLVM integration design comes from Laszlo Szekeres.
 
- */
+   This code is the rewrite of afl-as.h's main_payload.
+*/
 
 #include "../config.h"
 #include "../types.h"
@@ -35,8 +42,8 @@
 #include <sys/types.h>
 
 /* This is a somewhat ugly hack for the experimental 'trace-pc-guard' mode.
- Basically, we need to make sure that the forkserver is initialized after
- the LLVM-generated runtime initialization pass, not before. */
+   Basically, we need to make sure that the forkserver is initialized after
+   the LLVM-generated runtime initialization pass, not before. */
 
 #ifdef USE_TRACE_PC
 #  define CONST_PRIO 5
@@ -44,132 +51,157 @@
 #  define CONST_PRIO 0
 #endif /* ^USE_TRACE_PC */
 
-//#define BRANCH_HIST_LEN		3
 
 /* Globals needed by the injected instrumentation. The __afl_area_initial region
- is used for instrumentation output before __afl_map_shm() has a chance to run.
- It will end up as .comm, so it shouldn't be too wasteful. */
+   is used for instrumentation output before __afl_map_shm() has a chance to run.
+   It will end up as .comm, so it shouldn't be too wasteful. */
 
-u8 __afl_val_initial[MAP_SIZE];
-u8 *__afl_val_ptr = __afl_val_initial;
+u8  __afl_area_initial[MAP_SIZE];
+u8* __afl_area_ptr = __afl_area_initial;
 
-u32 __afl_idx_initial[MAP_SIZE];
-u32 *__afl_idx_ptr = __afl_idx_initial;
+u32  __afl_idx_initial[MAP_SIZE];
+u32* __afl_idx_ptr = __afl_idx_initial;
 
-__thread u32 __afl_prev_loc1 = LOC1_INIT;
-__thread u32 __afl_prev_loc2 = LOC2_INIT;
-__thread u32 __afl_prev_loc3 = LOC3_INIT;
+__thread u32 __afl_prev_loc1 = 3;
+__thread u32 __afl_prev_loc2 = 15;
+__thread u32 __afl_prev_loc3 = 27;
+
 
 /* Running in persistent mode? */
 
 static u8 is_persistent;
 
+
 /* SHM setup. */
 
 static void __afl_map_shm(void) {
 
-	u8 *id_str = getenv(SHM_VAL_ENV_VAR);
-	if (id_str) {
-		u32 shm_id = atoi(id_str);
-		__afl_val_ptr = shmat(shm_id, NULL, 0);
-		if (__afl_val_ptr == (void*) -1)
-			_exit(1);
-	}
-	id_str = getenv(SHM_IDX_ENV_VAR);
-	if (id_str) {
-		u32 shm_id = atoi(id_str);
-		__afl_idx_ptr = shmat(shm_id, NULL, 0);
-		if (__afl_idx_ptr == (void*) -1)
-			_exit(1);
-	}
+  u8 *id_str = getenv(SHM_VAL_ENV_VAR);
+
+  /* If we're running under AFL, attach to the appropriate region, replacing the
+     early-stage __afl_area_initial region that is needed to allow some really
+     hacky .init code to work correctly in projects such as OpenSSL. */
+
+  if (id_str) {
+
+    u32 shm_id = atoi(id_str);
+
+    __afl_area_ptr = shmat(shm_id, NULL, 0);
+
+    /* Whooooops. */
+
+    if (__afl_area_ptr == (void *)-1) _exit(1);
+
+    /* Write something into the bitmap so that even with low AFL_INST_RATIO,
+       our parent doesn't give up on us. */
+
+    //__afl_area_ptr[0] = 1;
+
+  }
+
+
+  id_str = getenv(SHM_IDX_ENV_VAR);
+
+    /* If we're running under AFL, attach to the appropriate region, replacing the
+       early-stage __afl_area_initial region that is needed to allow some really
+       hacky .init code to work correctly in projects such as OpenSSL. */
+
+    if (id_str) {
+
+      u32 shm_id = atoi(id_str);
+
+      __afl_idx_ptr = shmat(shm_id, NULL, 0);
+
+      /* Whooooops. */
+
+      if (__afl_idx_ptr == (void *)-1) _exit(1);
+
+      //__afl_idx_ptr[0] = 1;
+    }
+
 }
+
 
 /* Fork server logic. */
 
 static void __afl_start_forkserver(void) {
 
-	static u8 tmp[4];
-	s32 child_pid;
+  static u8 tmp[4];
+  s32 child_pid;
 
-	u8 child_stopped = 0;
+  u8  child_stopped = 0;
 
-	/* Phone home and tell the parent that we're OK. If parent isn't there,
-	 assume we're not running in forkserver mode and just execute program. */
+  /* Phone home and tell the parent that we're OK. If parent isn't there,
+     assume we're not running in forkserver mode and just execute program. */
 
-	if (write(FORKSRV_FD + 1, tmp, 4) != 4)
-		return;
+  if (write(FORKSRV_FD + 1, tmp, 4) != 4) return;
 
-	while (1) {
+  while (1) {
 
-		u32 was_killed;
-		int status;
+    u32 was_killed;
+    int status;
 
-		/* Wait for parent by reading from the pipe. Abort if read fails. */
+    /* Wait for parent by reading from the pipe. Abort if read fails. */
 
-		if (read(FORKSRV_FD, &was_killed, 4) != 4)
-			_exit(1);
+    if (read(FORKSRV_FD, &was_killed, 4) != 4) _exit(1);
 
-		/* If we stopped the child in persistent mode, but there was a race
-		 condition and afl-fuzz already issued SIGKILL, write off the old
-		 process. */
+    /* If we stopped the child in persistent mode, but there was a race
+       condition and afl-fuzz already issued SIGKILL, write off the old
+       process. */
 
-		if (child_stopped && was_killed) {
-			child_stopped = 0;
-			if (waitpid(child_pid, &status, 0) < 0)
-				_exit(1);
-		}
+    if (child_stopped && was_killed) {
+      child_stopped = 0;
+      if (waitpid(child_pid, &status, 0) < 0) _exit(1);
+    }
 
-		if (!child_stopped) {
+    if (!child_stopped) {
 
-			/* Once woken up, create a clone of our process. */
+      /* Once woken up, create a clone of our process. */
 
-			child_pid = fork();
-			if (child_pid < 0)
-				_exit(1);
+      child_pid = fork();
+      if (child_pid < 0) _exit(1);
 
-			/* In child process: close fds, resume execution. */
+      /* In child process: close fds, resume execution. */
 
-			if (!child_pid) {
+      if (!child_pid) {
 
-				close(FORKSRV_FD);
-				close(FORKSRV_FD + 1);
-				return;
+        close(FORKSRV_FD);
+        close(FORKSRV_FD + 1);
+        return;
+  
+      }
 
-			}
+    } else {
 
-		} else {
+      /* Special handling for persistent mode: if the child is alive but
+         currently stopped, simply restart it with SIGCONT. */
 
-			/* Special handling for persistent mode: if the child is alive but
-			 currently stopped, simply restart it with SIGCONT. */
+      kill(child_pid, SIGCONT);
+      child_stopped = 0;
 
-			kill(child_pid, SIGCONT);
-			child_stopped = 0;
+    }
 
-		}
+    /* In parent process: write PID to pipe, then wait for child. */
 
-		/* In parent process: write PID to pipe, then wait for child. */
+    if (write(FORKSRV_FD + 1, &child_pid, 4) != 4) _exit(1);
 
-		if (write(FORKSRV_FD + 1, &child_pid, 4) != 4)
-			_exit(1);
+    if (waitpid(child_pid, &status, is_persistent ? WUNTRACED : 0) < 0)
+      _exit(1);
 
-		if (waitpid(child_pid, &status, is_persistent ? WUNTRACED : 0) < 0)
-			_exit(1);
+    /* In persistent mode, the child stops itself with SIGSTOP to indicate
+       a successful run. In this case, we want to wake it up without forking
+       again. */
 
-		/* In persistent mode, the child stops itself with SIGSTOP to indicate
-		 a successful run. In this case, we want to wake it up without forking
-		 again. */
+    if (WIFSTOPPED(status)) child_stopped = 1;
 
-		if (WIFSTOPPED(status))
-			child_stopped = 1;
+    /* Relay wait status to pipe, then loop back. */
 
-		/* Relay wait status to pipe, then loop back. */
+    if (write(FORKSRV_FD + 1, &status, 4) != 4) _exit(1);
 
-		if (write(FORKSRV_FD + 1, &status, 4) != 4)
-			_exit(1);
-
-	}
+  }
 
 }
+
 
 /* A simplified persistent mode handler, used as explained in README.llvm. */
 
@@ -187,10 +219,10 @@ int __afl_persistent_loop(unsigned int max_cnt) {
 
     if (is_persistent) {
 
-      memset(__afl_val_ptr, 0, __afl_idx_ptr[0]);
-      __afl_prev_loc1 = LOC1_INIT;
-      __afl_prev_loc2 = LOC2_INIT;
-      __afl_prev_loc3 = LOC3_INIT;
+      memset(__afl_area_ptr, 0, __afl_idx_ptr[0]);
+      __afl_prev_loc1 = 3;
+      __afl_prev_loc2 = 15;
+      __afl_prev_loc3 = 27;
     }
 
     cycle_cnt  = max_cnt;
@@ -205,9 +237,9 @@ int __afl_persistent_loop(unsigned int max_cnt) {
 
       raise(SIGSTOP);
 
-      __afl_prev_loc1 = LOC1_INIT;
-      __afl_prev_loc2 = LOC2_INIT;
-      __afl_prev_loc3 = LOC3_INIT;
+      __afl_prev_loc1 = 3;
+      __afl_prev_loc2 = 15;
+      __afl_prev_loc3 = 27;
 
       return 1;
 
@@ -217,7 +249,7 @@ int __afl_persistent_loop(unsigned int max_cnt) {
          follows the loop is not traced. We do that by pivoting back to the
          dummy output region. */
 
-      __afl_val_ptr = __afl_val_initial;
+      __afl_area_ptr = __afl_area_initial;
       __afl_idx_ptr = __afl_idx_initial;
 
     }
@@ -228,76 +260,82 @@ int __afl_persistent_loop(unsigned int max_cnt) {
 
 }
 
+
 /* This one can be called from user code when deferred forkserver mode
- is enabled. */
+    is enabled. */
 
 void __afl_manual_init(void) {
 
-	static u8 init_done;
+  static u8 init_done;
 
-	if (!init_done) {
-		__afl_map_shm();
-		__afl_start_forkserver();
-		init_done = 1;
-	}
+  if (!init_done) {
+
+    __afl_map_shm();
+    __afl_start_forkserver();
+    init_done = 1;
+
+  }
+
 }
+
 
 /* Proper initialization routine. */
 
 __attribute__((constructor(CONST_PRIO))) void __afl_auto_init(void) {
 
-	is_persistent = !!getenv(PERSIST_ENV_VAR);
+  is_persistent = !!getenv(PERSIST_ENV_VAR);
 
-	if (getenv(DEFER_ENV_VAR))
-		return;
+  if (getenv(DEFER_ENV_VAR)) return;
 
-	__afl_manual_init();
+  __afl_manual_init();
 
 }
+
 
 /* The following stuff deals with supporting -fsanitize-coverage=trace-pc-guard.
- It remains non-operational in the traditional, plugin-backed LLVM mode.
- For more info about 'trace-pc-guard', see README.llvm.
+   It remains non-operational in the traditional, plugin-backed LLVM mode.
+   For more info about 'trace-pc-guard', see README.llvm.
 
- The first function (__sanitizer_cov_trace_pc_guard) is called back on every
- edge (as opposed to every basic block). */
+   The first function (__sanitizer_cov_trace_pc_guard) is called back on every
+   edge (as opposed to every basic block). */
 
-void __sanitizer_cov_trace_pc_guard(uint32_t* guard) {
-  __afl_val_ptr[*guard]++;
-}
+//void __sanitizer_cov_trace_pc_guard(uint32_t* guard) {
+// __afl_area_ptr[*guard]++;
+//}
+
 
 /* Init callback. Populates instrumentation IDs. Note that we're using
- ID of 0 as a special value to indicate non-instrumented bits. That may
- still touch the bitmap, but in a fairly harmless way. */
+   ID of 0 as a special value to indicate non-instrumented bits. That may
+   still touch the bitmap, but in a fairly harmless way. */
 
-void __sanitizer_cov_trace_pc_guard_init(uint32_t* start, uint32_t* stop) {
-
-  u32 inst_ratio = 100;
-  u8* x;
-
-  if (start == stop || *start) return;
-
-  x = getenv("AFL_INST_RATIO");
-  if (x) inst_ratio = atoi(x);
-
-  if (!inst_ratio || inst_ratio > 100) {
-    fprintf(stderr, "[-] ERROR: Invalid AFL_INST_RATIO (must be 1-100).\n");
-    abort();
-  }
+//void __sanitizer_cov_trace_pc_guard_init(uint32_t* start, uint32_t* stop) {
+//
+//  u32 inst_ratio = 100;
+//  u8* x;
+//
+//  if (start == stop || *start) return;
+//
+//  x = getenv("AFL_INST_RATIO");
+//  if (x) inst_ratio = atoi(x);
+//
+//  if (!inst_ratio || inst_ratio > 100) {
+//    fprintf(stderr, "[-] ERROR: Invalid AFL_INST_RATIO (must be 1-100).\n");
+//    abort();
+//  }
 
   /* Make sure that the first element in the range is always set - we use that
      to avoid duplicate calls (which can happen as an artifact of the underlying
      implementation in LLVM). */
 
-  *(start++) = R(MAP_SIZE - 1) + 1;
-
-  while (start < stop) {
-
-    if (R(100) < inst_ratio) *start = R(MAP_SIZE - 1) + 1;
-    else *start = 0;
-
-    start++;
-
-  }
-
-}
+//  *(start++) = R(MAP_SIZE - 1) + 1;
+//
+//  while (start < stop) {
+//
+//    if (R(100) < inst_ratio) *start = R(MAP_SIZE - 1) + 1;
+//    else *start = 0;
+//
+//    start++;
+//
+//  }
+//
+//}
